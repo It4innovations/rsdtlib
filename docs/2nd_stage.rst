c0f2a1a712b69cd8e5be8867720df760a80d1f7c
Georg Zitzlsberger <georg.zitzlsberger@vsb.cz>
2023-03-11 08:51:21 +0100

    Be more clear about the tile deadzones.

--- docs/2nd_stage.rst
+++ docs/2nd_stage.rst
@@ -7,6 +7,8 @@ The workflow for the 2nd and 3rd stages with exposed parameters is illustrated b
 
 In case of overlapping tiles (``deadzone_x > 0`` and/or ``deadzone_y > 0``), the image below shows the tile arrangement. The example only considers the neighboring tiles on the left/bottom of the blue tile. The same applies for neighboring tiles in the other directions.
 
+Note: The overlap of tiles is always ``deadzone_[x|y] * 2`` due to symmetries.
+
 .. figure:: ../images/tiles_overlap.png
 
 .. currentmodule:: rsdtlib
--- lib/rsdtlib.py
+++ lib/rsdtlib.py
@@ -650,10 +650,10 @@ class Stack:
     :param bands_opt: Number of optical multispectral bands (:math:`b_{OPT}`)
     :type bands_opt: int
     :param deadzone_x: Overlap tiles on x-axis so that neighboring tiles share
-        ``2*deadzone_x`` pixels per side.
+        ``2*deadzone_x`` pixels per side (default = ``0``)
     :type deadzone_x: int
     :param deadzone_y: Overlap tiles on y-axis so that neighboring tiles share
-        ``2*deadzone_y`` pixels per side.
+        ``2*deadzone_y`` pixels per side (default = ``0``)
     :type deadzone_y: int
 
     **Example:**
